12a4122fe90ddd03a33fa89f844dcc1f81664141
Hussein Alamutu <husseinalamutu@gmail.com>
2023-05-26 16:38:42 +0100

    added dns

--- main.tf
+++ main.tf
@@ -4,7 +4,21 @@ resource "aws_instance" "server" {
   instance_type          = each.key
 }
 
+resource "aws_route53_zone" "zone" {
+  count = length(var.domain_names)
+  name = var.domain_names[count.index]
+}
+resource "aws_route53_record" "dns_records" {
+  count = length(var.domain_names)
+  zone_id = aws_route53_zone.zone[count.index].zone_id
+  name = aws_route53_zone.zone[count.index].name
+  type = "A"
+  ttl = 300
+  records = ["1.2.3.4"]
+}
+ 
+
 resource "aws_s3_bucket" "bucket" {
   bucket = "manualbucket-importtf"
 }
- 
+
--- var.tf
+++ var.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   type = list(string)
   default = ["t2.micro", "t3.small", "t2.small", "t3.micro"]
 }
+
+variable "domain_names" {
+  type = list(string)
+  default = ["alamztech.com", "alamzglobalenterprise.com", "alamzempire.com", "alamzbrandmedia.com", "alamzhub.com"]
+}
